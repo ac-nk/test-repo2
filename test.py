fa405cf1d7dfb2953b9b52221ed13fef25b2cd97
neha-k123 <nehakul01@gmail.com>
2021-12-03 21:51:27 -0500

    resolved merge conflict

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
 import numpy as np
 
-<<<<<<< HEAD
 x3 = np.random.randint(3, size=(3, 4, 5))  # Three-dimensional array
 print(x3)
 print("asdfghfdsdfghjhgfds")
-=======
-x3 = np.random.randint(12, size=(3, 4, 5))  # Three-dimensional array
-print(x3)
-print("hello")
->>>>>>> origin/main
